9cf443edd5c310ee8be78c7b5d808e1aa943d0d2
Burak AKCAN <akcanburak93@outlook.com>
2025-04-05 22:48:30 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -7,22 +7,17 @@ let package = Package(
     name: "NFCPassportReader",
     platforms: [.iOS("15.0")],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NFCPassportReader",
             targets: ["NFCPassportReader"]),
     ],
     dependencies: [],
-    
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NFCPassportReader",
-            dependencies: ["OpenSSL"]),
+            dependencies: []),
         .testTarget(
             name: "NFCPassportReaderTests",
-            dependencies: ["NFCPassportReader", "OpenSSL"]),
+            dependencies: ["NFCPassportReader"])
     ]
 )
-
